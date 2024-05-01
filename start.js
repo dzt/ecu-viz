eefb5b220b8b3d3ed91ac509f7c030dc3c4b3e15
Peter Soboyejo <thepcmrtim@gmail.com>
2024-05-01 11:14:10 -0400

    Init

---
+++ start.js
@@ -0,0 +1,25 @@
+const s4_example = require('./input_examples/s4.json');
+const lib = require('./lib/index.js');
+const fs = require('fs');
+
+const YAML = require('json-to-pretty-yaml');
+
+const generateDiagram = (input) => {
+
+    let connectors = lib.createConnectors(input);
+    let cables = lib.createCables(connectors.summary, input);
+    let connections = lib.createConnections(connectors, cables, input);
+
+    return { connectors: connectors.data, cables, connections };
+}
+
+let output = generateDiagram(s4_example);
+let data = YAML.stringify(output)
+
+fs.writeFile('output.yaml', data, function(error) {
+    if(error) { 
+        console.log('[write auth]: ' + err);
+    } else {
+      console.log('[write auth]: success');
+    }
+});
