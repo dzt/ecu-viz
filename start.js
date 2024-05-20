8d25287287c1a6d3bb94e265ad22652d4ddaa531
Peter Soboyejo <thepcmrtim@gmail.com>
2024-05-20 11:53:40 -0400

    misc

--- start.js
+++ start.js
@@ -17,10 +17,19 @@ const generateDiagram = (input) => {
 let output = generateDiagram(s4_example);
 // console.dir(output)
 let data = YAML.stringify(output)
+
 fs.writeFile('out/output.yaml', data, function(error) {
     if(error) {
         console.log('[write auth]: ' + err);
     } else {
-      console.log('[write auth]: success');
+      console.log('[write auth]: success (output.yaml)');
+    }
+});
+
+fs.writeFile('out/output.json', JSON.stringify(output, null, 4), function(error) {
+    if(error) {
+        console.log('[write auth]: ' + err);
+    } else {
+      console.log('[write auth]: success (output.json)');
     }
 });
