d156414ba98e27b10d5207dcd92b7e25b96e57c6
Peter Soboyejo <thepcmrtim@gmail.com>
2024-05-27 19:48:30 -0400

    S13 Definitions

--- start.js
+++ start.js
@@ -1,4 +1,4 @@
-const s4_example = require('./input_examples/s5-atom.json');
+const s4_example = require('./input_examples/s13-monsoon.json');
 const lib = require('./lib/index.js');
 const fs = require('fs');
 
